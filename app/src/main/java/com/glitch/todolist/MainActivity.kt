5757020cf897d7d7181f4fce139ada6890fff2dc
wozverine <wozverine@users.noreply.github.com>
2023-09-19 23:54:21 +0300

    build fixed

--- app/src/main/java/com/glitch/todolist/MainActivity.kt
+++ app/src/main/java/com/glitch/todolist/MainActivity.kt
@@ -40,15 +40,6 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
